f204fdca0893efcbb6f83579789494c9d81bde96
Wojtek Gumuła <voodkapl@gmail.com>
2016-03-06 20:42:32 +0100

    sql dumps

---
+++ sql/event_scheduler_invitation.sql
@@ -0,0 +1,6 @@
+INSERT INTO event_scheduler.invitation (id, user_id, event_id, accepted) VALUES (1, 1, 1, 0);
+INSERT INTO event_scheduler.invitation (id, user_id, event_id, accepted) VALUES (2, 2, 2, 1);
+INSERT INTO event_scheduler.invitation (id, user_id, event_id, accepted) VALUES (3, 2, 3, 0);
+INSERT INTO event_scheduler.invitation (id, user_id, event_id, accepted) VALUES (4, 3, 1, 1);
+INSERT INTO event_scheduler.invitation (id, user_id, event_id, accepted) VALUES (5, 3, 2, 1);
+INSERT INTO event_scheduler.invitation (id, user_id, event_id, accepted) VALUES (6, 3, 3, 0);
